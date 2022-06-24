98bae73bbf0cb6a4c9bbc1db938fad55be8ef980
Jeff <98377720+Jeff-Nibbs@users.noreply.github.com>
2022-06-23 18:26:19 -0700

    first commit

---
+++ src/component/footer/Footer.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoTwitter } from "react-icons/io";
+
+function Footer() {
+  return (
+    <footer id="footer">
+      <a href="#header" className="footer__logo">
+        Daniel Nibbs
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#header">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="">
+          <FaFacebookF />
+        </a>
+        <a href="">
+          <FiInstagram />
+        </a>
+        <a href="">
+          <IoLogoTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Daniel Nibbs. All rights reserved.</small>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
